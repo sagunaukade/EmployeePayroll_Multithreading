6f673b489de47b7d1624e0eb13365d5d4202ec34
sagunaukade <sagunaukade96@gmail.com>
2022-03-08 23:14:53 +0530

    [Saguna] Welcome Message

---
+++ EmployeePayroll_Multithreading/EmployeePayroll_Multithreading/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll_Multithreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Payroll Using Multithreading");
+            Console.ReadLine();
+        }
+    }
+}
